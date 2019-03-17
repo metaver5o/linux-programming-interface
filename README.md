eb06c962ef55c30495f4e0408a187cc3b88c6486
Joshua Goller <joshua.goller@uber.com>
2019-03-17 12:43:13 -0700

    clean up makefile and organize exercises in chapter 6; bump readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # linux-programming-interface
 Reading notes from Kerrisk's "The Linux Programming Interface"
+
+# Todo:
+- 4
+  - 4.2
+- 6
+  - 6-2.
+  - 6-3.
+- 7
+  - 7-1
+- 29
+  - 29-1
+  - 29-2
--- ch-6/examples/wrong_write.c
+++ ch-6/examples/wrong_write.c
@@ -3,12 +3,15 @@
 int main() {
 #pragma clang diagnostic ignored "-Warray-bounds"
   char data[1];
-  // printf("Accessing the 4095th byte from data...\n");
-  // data[4095] = 'a';
-  // printf("data[4095]: %c\n", data[4095]);
+  /*
+  printf("Accessing the 4095th byte from data...\n");
+  data[4095] = 'a';
+  printf("data[4095]: %c\n", data[4095]);
+    */
   printf(
       "Now trying address far from data (this will usually cause a SIGSEGV "
       "if above lines are commented out).\n");
   data[100000] = 'a';
+
   return 0;
 }
